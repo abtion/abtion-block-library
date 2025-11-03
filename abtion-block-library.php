e320cea5beb60c1adab2ad9f4f4bd9fe3851ca56
Zarko <zarko.dorian@gmail.com>
2025-11-03 13:28:52 +0100

    Add script to build and load blocks

--- abtion-block-library.php
+++ abtion-block-library.php
@@ -14,3 +14,32 @@
  *
  * @package           abtion-block-library
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Define plugin constants.
+ */
+define( 'ABTION_BLOCK_LIBRARY_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ABTION_BLOCK_LIBRARY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Registers multiple block types from metadata loaded from a file.
+ */
+function abtion_block_library_register_blocks() {
+	$build_dir = ABTION_BLOCK_LIBRARY_PLUGIN_PATH . '/build/blocks';
+
+	if ( ! file_exists( $build_dir ) ) {
+		return;
+	}
+
+	$block_json_files = glob( $build_dir . '/*/block.json' );
+
+	foreach ( $block_json_files as $block_json_file ) {
+		register_block_type( dirname( $block_json_file ) );
+	}
+}
+
+add_action( 'init', 'abtion_block_library_register_blocks' );
