e6835f2fb16bf4e3163e5cf8f2bb1b0414041756
Petya Naydenova Ferreira <pn@abtion.com>
2025-11-25 14:46:05 +0100

    add build files

--- build/blocks/slider-slide/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '343e8d4c9aa01b617b07', 'type' => 'module');
--- build/blocks/slider-slide/view.js
+++
@@ -1,7 +0,0 @@
-var __webpack_exports__ = {};
-/*!*****************************************!*\
-  !*** ./src/blocks/slider-slide/view.js ***!
-  \*****************************************/
-console.log('hello from a SINGLE SLIDE inside slider');
-
-//# sourceMappingURL=view.js.map
