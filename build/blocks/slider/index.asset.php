a7403c98aea84a8ca52fc8035674946407642caf
Petya Naydenova Ferreira <pn@abtion.com>
2025-12-02 12:09:07 +0100

    add build files

--- build/blocks/slider/index.asset.php
+++ build/blocks/slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '619e3f80078568f38f83');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1807569efa25b2e17be1');
