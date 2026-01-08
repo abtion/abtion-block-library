36e694bd977573107a1fa665e9fbecfc2b720b98
Petya Naydenova Ferreira <pn@abtion.com>
2026-01-08 14:19:28 +0100

    add build files

--- build/blocks/multi-switch-panel/index.asset.php
+++ build/blocks/multi-switch-panel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-element'), 'version' => 'f5a282c2a0d67cca145b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-data', 'wp-element'), 'version' => '5b9924c6565e2f7d4794');
